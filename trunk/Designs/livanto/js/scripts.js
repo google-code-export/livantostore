8af7ae3095e701e2a63e34facdcf03de85ac1f15
sujprogrammer@gmail.com <sujprogrammer@gmail.com@a7285c99-2a5c-06c2-59d6-47a755b8e81c>
2014-04-06 08:52:06 +0000

    added for first time

---
+++ trunk/Designs/livanto/js/scripts.js
@@ -0,0 +1,56 @@
+var Script = function () {
+
+//    sidebar dropdown menu
+
+    jQuery('#sidebar .sub-menu > a').click(function () {
+        var last = jQuery('.sub-menu.open', $('#sidebar'));
+        last.removeClass("open");
+        jQuery('.arrow', last).removeClass("open");
+        jQuery('.sub', last).slideUp(200);
+        var sub = jQuery(this).next();
+        if (sub.is(":visible")) {
+            jQuery('.arrow', jQuery(this)).removeClass("open");
+            jQuery(this).parent().removeClass("open");
+            sub.slideUp(200);
+        } else {
+            jQuery('.arrow', jQuery(this)).addClass("open");
+            jQuery(this).parent().addClass("open");
+            sub.slideDown(200);
+        }
+    });
+
+//    sidebar toggle
+
+    $('.icon-reorder').click(function () {
+        if ($('#sidebar > ul').is(":visible") === true) {
+            $('#main-content').css({
+                'margin-left': '0px'
+            });
+            $('#sidebar').css({
+                'margin-left': '-180px'
+            });
+            $('#sidebar > ul').hide();
+            $("#container").addClass("sidebar-closed");
+        } else {
+            $('#main-content').css({
+                'margin-left': '180px'
+            });
+            $('#sidebar > ul').show();
+            $('#sidebar').css({
+                'margin-left': '0'
+            });
+            $("#container").removeClass("sidebar-closed");
+        }
+    });
+
+// custom scrollbar
+    $(".sidebar-scroll").niceScroll({styler:"fb",cursorcolor:"#4A8BC2", cursorwidth: '5', cursorborderradius: '0px', background: '#404040', cursorborder: ''});
+
+  
+
+
+
+
+}();
+
+
